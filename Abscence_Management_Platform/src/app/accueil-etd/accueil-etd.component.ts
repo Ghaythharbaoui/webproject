0719e85a6a3a66968bb7a42c1f6dc64838b98509
AlaMadani <alemedeni0@gmail.com>
2025-03-03 17:57:01 +0100

    commit 0.0.3

--- Abscence_Management_Platform/src/app/accueil-etd/accueil-etd.component.ts
+++ Abscence_Management_Platform/src/app/accueil-etd/accueil-etd.component.ts
@@ -1,11 +1,39 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-accueil-etd',
-  imports: [],
+  imports: [FormsModule,CommonModule],
   templateUrl: './accueil-etd.component.html',
   styleUrl: './accueil-etd.component.css'
 })
 export class AccueilEtdComponent {
 
+  classes = ['Classe A', 'Classe B', 'Classe C'];
+  specs = ['Spec 1', 'Spec 2'];
+  groups = ['Groupe 1', 'Groupe 2'];
+  teachers = ['Prof X', 'Prof Y'];
+  labels = ['Label 1', 'Label 2'];
+
+  // Variables liées aux <select> via [(ngModel)]
+  selectedClass = '';
+  selectedSpec = '';
+  selectedGroup = '';
+  selectedTeacher = '';
+  selectedLabel = '';
+
+  // Liste des éléments épinglés
+  pinnedItems = [
+    { content: 'Absence de M. Dupont'},
+    { content: 'Rattrapage - Salle 101'},
+    { content: 'Absence de Mme Martin' },
+    { content: 'Absence de M. Dupont' },
+    { content: 'Rattrapage - Salle 101' },
+    { content: 'Absence de Mme Martin'},
+    { content: 'Absence de M. Dupont' },
+    { content: 'Rattrapage - Salle 101'},
+    { content: 'Absence de Mme Martin' },
+  ];
+
+  constructor() {}
 }
